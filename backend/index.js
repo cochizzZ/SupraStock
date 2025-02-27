2369da9ec1dad1b622b17070ef12e7127cd3f403
JuanRM <Juan2013rico@gmail.com>
2025-02-27 11:51:32 -0500

    Actualización del frontend y agregado backend

---
+++ backend/index.js
@@ -0,0 +1,139 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// Conexion con la base de datos de MongoDB
+mongoose.connect("mongodb+srv://JuanRM:JuanTDP10@stp.jlm2k.mongodb.net/suprastock");
+
+// Creacion de API
+
+app.get("/",(req,res)=>{
+    res.send("Express App is Running")
+})
+
+// Creacion de API para subir imagenes
+
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage:storage})
+
+// Creacion de API para subir imagenes
+
+app.use('/images',express.static('upload/images'))
+
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+// Esquema para la creación de productos
+
+const Product = mongoose.model("Product",{
+    id:{
+        type:Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    available:{
+        type:Boolean,
+        default:true,
+    },
+})
+
+app.post('/addproduct',async(req,res)=>{
+    let products= await Product.find({});
+    let id;
+    if(products.length>0)
+    {
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }
+    else
+    {
+        id = 1;
+    }
+    const product = new Product({
+
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+// Creacion de API para Remover Productos
+
+app.post('/removeproduct',async(req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
+
+// Creación de API para obtener todos los productos
+
+app.get('/allproducts',async(req,res)=>{
+    let products = await Product.find({});
+    console.log("All products Fetched");
+    res.send(products);
+})
+
+app.listen(port,(error)=>{
+    if (!error) {
+        console.log("Server Running on Port"+port)
+    }
+    else
+    {
+        console.log("Error : "+error)
+    }
+})
