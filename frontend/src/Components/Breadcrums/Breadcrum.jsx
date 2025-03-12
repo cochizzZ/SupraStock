0903879807b3298d8d2542067ea511fb528fd145
NataCOF <nataliak_ospina@soy.sena.edu.co>
2025-03-11 23:12:14 -0500

    Breadcrum actualizado

--- frontend/src/Components/Breadcrums/Breadcrum.jsx
+++ frontend/src/Components/Breadcrums/Breadcrum.jsx
@@ -6,7 +6,7 @@ const Breadcrum = (props) => {
     const {product} = props;
   return (
     <div className='breadcrum'>
-      HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
+      Inicio <img src={arrow_icon} alt="" /> Tienda <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
     </div>
   )
 }
