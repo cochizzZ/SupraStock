ff122056a4a312ecc9ca00b668578b9a5e191cbb
JuanR <Juan2013rico@gmail.com>
2025-03-13 12:22:16 -0500

    Se agrega un icono de agregado rapido de productos

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
-import './Item.css'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react';
+import './Item.css';
+import { Link } from 'react-router-dom';
+import { ShopContext } from '../../Context/ShopContext';
+import addToCartIcon from '../Assets/add_to_cart_icon.png'; // Asegúrate de tener este ícono
 
 const Item = (props) => {
+  const { addToCart } = useContext(ShopContext);
+
+  const handleAddToCart = () => {
+    addToCart(props.id);
+  };
+
   return (
     <div className='item'>
-        <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
-        <p>{props.name}</p>
-        <div className="item-prices">
-            <div className="item-price-new">
-                ${props.new_price}
-            </div>
-            <div className="item-price-old">
-                ${props.old_price}
-            </div>
+      <Link to={`/product/${props.id}`}>
+        <img onClick={() => window.scrollTo(0, 0)} src={props.image} alt="" />
+      </Link>
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">
+          ${props.new_price}
         </div>
+        <div className="item-price-old">
+          ${props.old_price}
+        </div>
+      </div>
+      <button onClick={handleAddToCart} className="add-to-cart-btn">
+        <img src={addToCartIcon} alt="Agregar al carrito" />
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
