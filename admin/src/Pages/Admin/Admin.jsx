3ea91120252897d459ce22555a62c403d2ab6cd0
Emmanuel <you@example.com>
2025-03-18 21:13:41 -0500

    Se modifica información en la página principal del Admin Panel

--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -21,8 +21,11 @@ const Admin = () => {
               <h1>Bienvenido al Panel de Administrador</h1>
               <p>Gestiona tus productos de manera fácil y eficiente.</p>
               <div className="admin-guide">
+                <p> - <strong>Dashboard:</strong> Ingresa en "Dashboard" para ver datos de la página de manera clara y organizada.</p>
                 <p> - <strong>Añadir Productos:</strong> Usa la opción "Agregar Producto" para añadir nuevos productos.</p>
                 <p> - <strong>Lista de Productos:</strong> Entra en "Lista de Productos" para visualizar o eliminar los productos existentes.</p>
+                <p> - <strong>Lista de Órdenes:</strong> Ingresa a "Lista de Órdenes" para gestionar órdenes registradas.</p>
+                <p> - <strong>Lista de Usuarios:</strong> Entra a "Lista de Usuarios" para gestionar usuarios de la página.</p>
                 <p> - <strong>Cerrar Sesión:</strong> Puedes salir en cualquier momento con la opción "Cerrar Sesión".</p>
               </div>
               <p className="admin-note">Usa el menú lateral para navegar entre las secciones.</p>
