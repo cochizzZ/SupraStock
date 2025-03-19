73c3d6b2613c78698eb050e2cb626081112718a4
RicoJM <112290541+RicoJM@users.noreply.github.com>
2025-03-18 19:06:42 -0500

    Cambios sobre las ordenes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
@@ -11,31 +12,34 @@ import men_banner from './Components/Assets/banner_mens.png';
 import women_banner from './Components/Assets/banner_women.png';
 import kid_banner from './Components/Assets/banner_kids.png';
 import UserProfile from './Pages/UserProfile';
-import UserOrders from './Components/UserOrders/UserOrders'; // Importar UserOrders
+import UserOrders from './Components/UserOrders/UserOrders';
+import Cartitems from './Components/Cartitems/Cartitems';
+import OrderForm from './Components/OrderForm/OrderForm';
+import { Order } from './Components/OrderForm/OrderForm';
 
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Shop />} />
-          <Route path="/login" element={<LoginSignup />} />
-          <Route path="/profile" element={<UserProfile />} />
-          <Route path="/orders" element={<UserOrders />} />
-          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />} />
-          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
-          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />} />
-          <Route path="/product" element={<Product />}>
-            <Route path=':productId' element={<Product />} />
-          </Route>
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/login' element={<LoginSignup />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Shop />} />
+                <Route path="/login" element={<LoginSignup />} />
+                <Route path="/profile" element={<UserProfile />} />
+                <Route path="/orders" element={<UserOrders />} />
+                <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />} />
+                <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
+                <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />} />
+                <Route path="/product" element={<Product />}>
+                    <Route path=':productId' element={<Product />} />
+                </Route>
+                <Route path='/cart' element={<Cart />} />
+                <Route path='/login' element={<LoginSignup />} />
+                <Route path="/" element={<Cartitems />} />
+                <Route path="/order" element={<OrderForm />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
+};
 
 export default App;
