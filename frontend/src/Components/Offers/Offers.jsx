5ed6e9649e91cebc86774418fbed423595661106
Emmanuel <you@example.com>
2025-03-20 20:05:22 -0500

    Componente de Ofertas Organizado

--- frontend/src/Components/Offers/Offers.jsx
+++ frontend/src/Components/Offers/Offers.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import './Offers.css'
+import React from 'react';
+import './Offers.css';
+import exclusive_image from '../Assets/exclusive_image.png'; 
 
 const Offers = () => {
   return (
     <div className='offers'>
       <div className="offers-left">
-        <h1>Ofertas</h1>
-        <h1>Exclusivas solo para ti</h1>
-        <p>SOLO LOS MEJORES PRODUCTOS</p>
-        <button>Mirar</button>
+        <h1> Ofertas Exclusivas solo para ti</h1>
+        <p>LOS MEJORES PRODUCTOS AL MEJOR PRECIO</p>
       </div>
       <div className="offers-right">
+        <img src={exclusive_image} alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Offers 
+export default Offers;
