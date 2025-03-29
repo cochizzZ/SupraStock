488c402192a8439b2c1cf913fa3677ab3c323e94
NataCOF <nataliak_ospina@soy.sena.edu.co>
2025-03-28 23:44:14 -0500

    Linea funcional

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,37 +1,46 @@
-import React, { useContext } from 'react'
-import './CSS/ShopCategory.css'
-import { ShopContext } from '../Context/ShopContext'
-import dropdown_icon from '../Components/Assets/dropdown_icon.png'
-import Item from '../Components/Item/Item'
+import React, { useContext } from 'react';
+import './CSS/ShopCategory.css';
+import { ShopContext } from '../Context/ShopContext';
+import dropdown_icon from '../Components/Assets/dropdown_icon.png';
+import Item from '../Components/Item/Item';
 
 const ShopCategory = (props) => {
-  const {all_product} = useContext(ShopContext);
+  const { all_product } = useContext(ShopContext);
+
+  // Filtrar los productos por categoría
+  const filteredProducts = all_product.filter(
+    (product) => product.category === props.category
+  );
+
   return (
     <div className='shop-category'>
       <img className='shopcategory-banner' src={props.banner} alt="" />
       <div className="shopcategory-indexSort">
         <p>
-          <span>Showing 1-12</span> out of 36 products
+          <span>Mostrando 1 - {Math.min(12, filteredProducts.length)}</span> de {filteredProducts.length} productos
         </p>
         <div className="shopcategory-sort">
           Sort by <img src={dropdown_icon} alt="" />
         </div>
       </div>
       <div className="shopcategory-products">
-        {all_product.map((item,i)=>{
-          if (props.category===item.category) {
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} sizes={item.sizes}/>
-          }
-          else{
-            return null;
-          }
-        })}
+        {filteredProducts.slice(0, 12).map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+            sizes={item.sizes}
+          />
+        ))}
       </div>
       <div className="shopcategory-loadmore">
         Ver más
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ShopCategory
+export default ShopCategory;
