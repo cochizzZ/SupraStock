26b5d9a578193c3da1d5149b3b7840e4774e480a
NataCOF <nataliak_ospina@soy.sena.edu.co>
2025-04-06 17:02:38 -0500

    Pruebas de estres

--- backend/tests/stress/register.test.js
+++ backend/tests/stress/register.test.js
@@ -3,9 +3,7 @@ import { sleep, check } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '10s', target: 100 }, // Ramp-up to 100 users over 5 seconds
-        { duration: '10s', target: 100 }, // Stay at 100 users for 10 seconds
-        { duration: '10s', target: 0 }, // Ramp-down to 0 users over 5 seconds
+        { duration: '100s', target: 1 }, // Ramp-up to 100 users over 5 seconds
     ],
     thresholds: {
         http_req_duration: ['p(95)<5000'], // 95% of requests should be below 500ms
@@ -16,7 +14,7 @@ export let options = {
 export default function () {
     const uniqueId  = `${__VU}_${__ITER}` ; // Replace with your API endpoint
     const payload = JSON.stringify({
-        name: `Alex_${uniqueId}`,
+        name: `AlexanderMoneque`,
         email: `Alex.p${uniqueId}@gmail.com`,
         password: `Mimamamemima3.4`
     });
@@ -27,7 +25,7 @@ export default function () {
         },
     };
 
-    let res = http.post("http://localhost:4000/login", payload, params);
+    let res = http.post("http://localhost:4000/signup", payload, params);
 
     const checkRest = check (res, {
         'respuesta status es 200': (r) => r.status === 200,
@@ -42,9 +40,12 @@ export default function () {
         }
     },
     }); 
+    const responseBody = JSON.parse(res.body);
+
     // Check the response status code
     if (!checkRest) {
-        console.error(`Fallo en la validacion del usuario ${res.status}`);
+        console.error(`Fallo en la validacion del usuario ${res.status},
+            ${responseBody.message}`);
     }
 
     sleep(1); // Sleep for 1 second between iterations
